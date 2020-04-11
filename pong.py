d0958c7d53431463f96a432abb85a73091ae8a4b
Jack Driscoll <jdriscoll98@ufl.edu>
2020-04-11 15:06:15 -0400

    initial commit

---
+++ pong.py
@@ -0,0 +1,32 @@
+# Simple python game 
+import turtle
+
+window = turtle.Screen()
+window.title("Pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+# Ball
+
+# Main game loop
+while True:
+    window.update()
+
